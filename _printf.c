a84b439f5c7b34aa98d79622955b5ec98fcebe01
Albinnich <albina_haji@hotmail.com>
2024-03-26 08:35:11 +0000

    First try

---
+++ _printf.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdarg.h>
+/**
+ * _printf - function that produces output acc to format
+ * @format: character string
+ * Return: number of characters written
+ */
+int _printf(const char *format, ...)
+{
+	int count = 0;
+	va_list args;
+
+	va_start(args, format);
+
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == 'c')
+			{
+				char c;
+
+				c = va_arg(args, int);
+				write(1, &c, 1);
+				count++;
+			}
+			else if (*format == 's')
+			{
+				char *str;
+
+				str = va_arg(args, char *);
+
+				while (*str != '\0')
+				{
+					write(1, str, 1);
+					str++;
+					count++;
+				}
+			}
+			else if (*format == '%')
+			{
+				write(1, "%", 1);
+				count++;
+			}
+		}
+		else
+		{
+			write(1, format, 1);
+				count++;
+		}
+		format++;
+	}
+	va_end(args);
+	return (count);
+}
